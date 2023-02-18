1e638b81a8cb87fb0558a287859be4ac3d68187c
yybgx <2398456592qq.com>
2023-02-19 00:44:18 +0800

    更换评论图标

--- pages/news_detail.vue
+++ pages/news_detail.vue
@@ -13,8 +13,8 @@
           <img src="~/assets/images/点赞选中.svg" class="tubiao" @click="addlike" v-if="isadd"/>
         </div>
          <div class="side"> <img src="~/assets/images/收藏 .svg" class="tubiao"/></div>
-         <div class="side"> <img src="~/assets/images/评论.svg" class="tubiao" @click="readComment"/></div>
-         <div class="side"> <img src="~/assets/images/转发.svg" class="tubiao"/></div>
+         <div class="side"> <img src="~/assets/images/note.svg" class="tubiao" @click="readComment"/></div>
+         <div class="side"> <img src="~/assets/images/评论.svg" class="tubiao"/></div>
          <hr>
          <div class="side"> <img src="~/assets/images/举报.svg" class="tubiao"/></div>
         </div>
